ebacc2d43af7d9fcb8b8bead38272977eeb49bd0
Angela Ku <angku88@gmail.com>
2023-11-06 14:52:40 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// Example 03: Scrolling States
+// Name: Angela Ku
+// Date: 2023-11-06
+
+'use strict'
+
+const config = {
+    parent: 'phaser-game',  // for info text
+    type: Phaser.WEBGL,     // for tinting
+    width: 400,
+    height: 300,
+    pixelArt: true,
+    zoom: 2,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    scene: [ Load, Play ]
+}
+
+const game = new Phaser.Game(config)
